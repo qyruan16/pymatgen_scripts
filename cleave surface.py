e8cfcde1b9624bb39f4e5dd0648e3848bc0e9b5b
qyruan16 <91582482+qyruan16@users.noreply.github.com>
2021-10-05 18:05:10 -0500

    Update cleave surface.py

--- cleave surface.py
+++ cleave surface.py
@@ -7,9 +7,9 @@ from matplotlib import pyplot as plt
 from pymatgen.io.vasp.inputs import Poscar
 
 mpr = MPRester()
-LiPO4 = mpr.get_materials_ids("LiPO4")
+LiFePO4 = mpr.get_materials_ids("LiFePO4")
 
-struct = mpr.get_structure_by_material_id(LiPO4[0])
+struct = mpr.get_structure_by_material_id(LiFePO4[0])
 slab = SlabGenerator(struct, (0, 0, 1), 10, 15, center_slab=True)
 
 
@@ -21,7 +21,7 @@ for n, slabs in enumerate(slab.get_slabs(bonds = {('P', 'O'): 3})):
     ax = fig.add_subplot(1, 1, 1)
     plot_slab(slabs, ax, adsorption_sites=True)
     plt.savefig(str(n) + '-LiFePO4-001.png', format='png')
-    open('POSCAR-LiPO4-' + str(n), 'w').write(str(Poscar(slabs)))
+    open('POSCAR-LiFePO4-' + str(n), 'w').write(str(Poscar(slabs)))
 
 # More complicated supercell
 # slabs.make_supercell([[1,1,0],
